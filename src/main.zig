d869ea543e5fe0bc89f6cd4e389c5e2ddf3f9bdd
Tesseract22 <caleb2222@qq.com>
2023-09-11 20:29:19 -0500

    fix path bug, add colored output

--- src/main.zig
+++ src/main.zig
@@ -4,10 +4,11 @@ const Ast = std.zig.Ast;
 const assert = std.debug.assert;
 const FileSet = std.AutoHashMap(std.fs.File.INode, void);
 
-
 const LVDistance = @import("distance.zig");
 const LevenshteinDistance = LVDistance.LevenshteinDistance;
 const LevenshteinDistanceOptions = LVDistance.LevenshteinDistanceOptions;
+
+const Color = @import("color.zig").Color;
 
 const stdout_file = std.io.getStdOut().writer();
 var bw = std.io.bufferedWriter(stdout_file);
@@ -22,12 +23,11 @@ fn tokenRender(tree: Ast, token_index: ?Ast.TokenIndex) []const u8 {
 fn PrintExpr(tree: Ast, type_index: Ast.Node.Index) !void {
     const first_token = tree.firstToken(type_index);
     const last_token = tree.lastToken(type_index);
-    for (first_token..last_token+1) |i| {
+    for (first_token..last_token + 1) |i| {
         _ = try stdout.print("{s}", .{tokenRender(tree, @intCast(i))});
     }
 }
 fn PrintFn(tree: Ast, fnProto: Ast.full.FnProto) !void {
-
     if (fnProto.visib_token) |v| {
         try stdout.print("{s} ", .{tokenRender(tree, v)});
     }
@@ -39,7 +39,7 @@ fn PrintFn(tree: Ast, fnProto: Ast.full.FnProto) !void {
         if (iterator.param_i != fnProto.ast.params.len) try stdout.print(", ", .{});
     }
     _ = try stdout.write(") ");
-    if (fnProto.ast.callconv_expr != 0)  {
+    if (fnProto.ast.callconv_expr != 0) {
         _ = try stdout.write("callconv(");
         try PrintExpr(tree, fnProto.ast.callconv_expr);
         _ = try stdout.write(") ");
@@ -68,7 +68,7 @@ fn ExactMatchType(a_tree: *Ast, b_tree: *Ast, a: Ast.Node.Index, b: Ast.Node.Ind
         if (a_tag != b_tag) return false;
         switch (a_tag) {
             .identifier => {
-                if (!std.mem.eql(u8, tokenRender(a_tree.*, @intCast(ai)) , tokenRender(b_tree.*, @intCast(bi)))) return false;
+                if (!std.mem.eql(u8, tokenRender(a_tree.*, @intCast(ai)), tokenRender(b_tree.*, @intCast(bi)))) return false;
             },
             else => {},
         }
@@ -101,7 +101,6 @@ fn FuzzyMatchIdentifier(allocator: std.mem.Allocator, a_tree: *Ast, b_tree: *Ast
     const a_token = a_tree.nodes.items(.main_token)[a];
     const b_token = a_tree.nodes.items(.main_token)[b];
     return LevenshteinDistance(allocator, tokenRender(a_tree, a_token), tokenRender(b_tree, b_token)) catch std.math.maxInt(u32);
-    
 }
 
 const TokenTag = std.zig.Token.Tag;
@@ -111,16 +110,11 @@ fn EqlTag(ctx: ?*anyopaque, a: TokenTag, b: TokenTag) bool {
     return a == b;
 }
 
-fn FuzzyMatchType(
-                allocator: std.mem.Allocator, 
-                a_tree: *Ast, b_tree: *Ast, 
-                a: Ast.Node.Index, b: Ast.Node.Index) 
-                u32 {
+fn FuzzyMatchType(allocator: std.mem.Allocator, a_tree: *Ast, b_tree: *Ast, a: Ast.Node.Index, b: Ast.Node.Index) u32 {
     const a_type = a_tree.tokens.items(.tag)[a_tree.firstToken(a)..a_tree.lastToken(a)];
     const b_type = b_tree.tokens.items(.tag)[b_tree.firstToken(b)..b_tree.lastToken(b)];
-    var d = LevenshteinDistanceOptions(TokenTag)(allocator, a_type, b_type, .{.eql=EqlTag}) catch return std.math.maxInt(u32);
-    d += LevenshteinDistance(allocator, tokenRender(a_tree.*, a_tree.lastToken(a)), tokenRender(b_tree.*, b_tree.lastToken(b)))
-                                                        catch return std.math.maxInt(u32);
+    var d = LevenshteinDistanceOptions(TokenTag)(allocator, a_type, b_type, .{ .eql = EqlTag }) catch return std.math.maxInt(u32);
+    d += LevenshteinDistance(allocator, tokenRender(a_tree.*, a_tree.lastToken(a)), tokenRender(b_tree.*, b_tree.lastToken(b))) catch return std.math.maxInt(u32);
     return d;
 }
 
@@ -133,16 +127,10 @@ fn FuzzyCostType(tree: Ast, a: Ast.Node.Index) u32 {
     return d;
 }
 
-fn FuzzyMatchFn(
-                allocator: std.mem.Allocator, 
-                a_tree: *Ast, b_tree: *Ast, 
-                a: Ast.full.FnProto, b: Ast.full.FnProto) 
-                u32 {
+fn FuzzyMatchFn(allocator: std.mem.Allocator, a_tree: *Ast, b_tree: *Ast, a: Ast.full.FnProto, b: Ast.full.FnProto) u32 {
     var dist: u32 = 0;
     if (a.name_token != null and b.name_token != null) {
-        dist += (LevenshteinDistance(allocator, 
-                                    tokenRender(a_tree.*, a.name_token), 
-                                    tokenRender(b_tree.*, b.name_token)) catch return std.math.maxInt(u32));
+        dist += (LevenshteinDistance(allocator, tokenRender(a_tree.*, a.name_token), tokenRender(b_tree.*, b.name_token)) catch return std.math.maxInt(u32));
     }
 
     dist += FuzzyMatchType(allocator, a_tree, b_tree, a.ast.return_type, b.ast.return_type);
@@ -150,14 +138,13 @@ fn FuzzyMatchFn(
     var ai = a.iterate(a_tree);
     var bi = b.iterate(b_tree);
     while (true) {
-
         const ap = ai.next();
         const bp = bi.next();
         if (ap == null and bp == null) {
             break;
         } else if (ap == null) {
             dist += FuzzyCostType(b_tree.*, bp.?.type_expr) * 2;
-        } else if (bp == null ){
+        } else if (bp == null) {
             dist += FuzzyCostType(a_tree.*, ap.?.type_expr) * 2;
         } else {
             const a_name = tokenRender(a_tree.*, ap.?.name_token);
@@ -172,7 +159,7 @@ fn FuzzyMatchFn(
 }
 
 const OpenError = std.fs.File.OpenError;
-const ParseError = error {
+const ParseError = error{
     ExpectFileArgument,
     ExpectMatchArgument,
     InvalidMatchSyntax,
@@ -208,16 +195,7 @@ fn PrintUsage() void {
     std.debug.print("{s: >8} search_string = \"fn [$fn_name]([$var_name]: $var_type, ...) $var_type\"\n", .{"where"});
 }
 
-
-fn SearchFile(
-    allocator: std.mem.Allocator, 
-    file: File, 
-    queue: *FnQueue, 
-    match_ast: *Ast, match_fn: Ast.full.FnProto, 
-    dir: *std.heap.FixedBufferAllocator, 
-    file_set: *FileSet,
-    depth: u32) 
-    !void {
+fn SearchFile(allocator: std.mem.Allocator, file: File, queue: *FnQueue, match_ast: *Ast, match_fn: Ast.full.FnProto, dir: *std.heap.FixedBufferAllocator, file_set: *FileSet, depth: u32) !void {
     const file_size = (file.stat() catch |e| {
         std.log.err("Cannot get file size", .{});
         return e;
@@ -230,46 +208,43 @@ fn SearchFile(
     _ = try file.readAll(source);
     var ast = try Ast.parse(allocator, source, .zig);
     defer ast.deinit(allocator);
-    for (ast.rootDecls()) |d| { 
+    for (ast.rootDecls()) |d| {
         var buffer: [1]Ast.Node.Index = undefined;
         var fn_proto = ast.fullFnProto(&buffer, d) orelse continue;
         const distance = FuzzyMatchFn(allocator, &ast, match_ast, fn_proto, match_fn);
-        std.log.debug("fn {s}() distance: {}", .{tokenRender(ast, fn_proto.name_token), distance});
+        std.log.debug("fn {s}() distance: {}", .{ tokenRender(ast, fn_proto.name_token), distance });
         if (distance > dist_limit) continue;
         const fn_result = try allocator.create(FnResult);
         const location = ast.tokenLocation(0, fn_proto.ast.fn_token);
-        const location_str = std.fmt.allocPrint(allocator, "{s}:{}:{}", .{dir.buffer[0..dir.end_index], location.line, location.column});
-        fn_result.* = .{.fnString = try FormatFn(allocator, ast, fn_proto), 
-                        .distance = distance, 
-                        .location = try location_str
-                        };
+        const location_str = std.fmt.allocPrint(allocator, "{s}:{}:{}", .{ dir.buffer[0..dir.end_index], location.line, location.column });
+        fn_result.* = .{ .fnString = try FormatFn(allocator, ast, fn_proto), .distance = distance, .location = try location_str };
         try queue.add(fn_result);
     }
     if (depth == 1) return;
-    const dir_curr = dir.end_index;
-    var temp_dir = try std.fs.openDirAbsolute(dir.buffer[0..dir_curr], .{});
-    std.log.debug("Currently in: {s}", .{dir.buffer[0..dir_curr]});
-    defer temp_dir.close();
+
+    const cwd_path = GetDirOfPath(dir.buffer[0..dir.end_index]);
+    dir.end_index = cwd_path.len;
+    var cwd = try std.fs.openDirAbsolute(cwd_path, .{});
+    std.log.debug("Currently in: {s}", .{cwd_path});
+    defer cwd.close();
     for (ast.nodes.items(.tag), 0..) |t, i| {
         switch (t) {
             .builtin_call_two,
-            .builtin_call, 
+            .builtin_call,
             => {
                 const token = tokenRender(ast, ast.nodes.items(.main_token)[i]);
                 if (std.mem.eql(u8, token, "@import")) {
                     const arg = ast.nodes.items(.data)[i];
                     const file_name_quote = tokenRender(ast, ast.nodes.items(.main_token)[arg.lhs]);
-                    const file_name = file_name_quote[1..file_name_quote.len - 1];
+                    const file_name = file_name_quote[1 .. file_name_quote.len - 1];
                     // std.log.debug("@import({s})", .{file_name});
                     if (std.mem.eql(u8, file_name, "root") or std.mem.eql(u8, file_name, "std") or std.mem.eql(u8, file_name, "builtin")) continue;
 
-                    var next_file = try temp_dir.openFile(file_name, .{.mode = .read_only});
+                    var next_file = try cwd.openFile(file_name, .{ .mode = .read_only });
                     defer next_file.close();
-                    _ = try std.fmt.allocPrint(dir.allocator(), "/{s}", .{GetDirOfPath(file_name)});
-                    defer dir.end_index = dir_curr;
+                    _ = try std.fmt.allocPrint(dir.allocator(), "/{s}", .{file_name});
+                    defer dir.end_index = cwd_path.len;
                     // std.log.debug("new dir: {s}", .{dir.buffer[0..dir.end_index]});
-
-
 
                     const inode = (try next_file.stat()).inode;
                     if (file_set.contains(inode)) {
@@ -278,19 +253,8 @@ fn SearchFile(
                         try file_set.put(inode, {});
                     }
                     std.log.debug("Searching next file: {s}", .{file_name});
-                    try SearchFile(
-                        allocator, 
-                        next_file, 
-                        queue, 
-                        match_ast, match_fn, 
-                        dir, 
-                        file_set,
-                        depth - 1);
-
-
+                    try SearchFile(allocator, next_file, queue, match_ast, match_fn, dir, file_set, depth - 1);
                 }
-                
-                
             },
             else => {},
         }
@@ -299,7 +263,6 @@ fn SearchFile(
 
 const dist_limit = 20;
 var recursive_depth: u32 = 1;
-
 
 pub fn main() !void {
     errdefer PrintUsage();
@@ -324,8 +287,8 @@ pub fn main() !void {
                 return e;
             };
         }
-    } 
-    
+    }
+
     const file_name = args[1];
     const match_string = args[2];
 
@@ -334,8 +297,6 @@ pub fn main() !void {
     // debugAst(match_ast);
     var match_fn_node: [1]Ast.Node.Index = undefined;
     const match_fn = eval_fn: {
-        
-        
         break :eval_fn for (match_ast.nodes.items(.tag), 0..) |t, i| {
             _ = t;
             match_fn_node[0] = @intCast(i);
@@ -348,7 +309,6 @@ pub fn main() !void {
     var path_buffer = [_]u8{0} ** 1024;
     var path_fba = std.heap.FixedBufferAllocator.init(&path_buffer);
 
-
     var file_set = FileSet.init(allocator);
     defer file_set.deinit();
     var file = find_file: {
@@ -358,16 +318,16 @@ pub fn main() !void {
             while (iterator.next()) |env_path| {
                 var temp_dir = std.fs.openDirAbsolute(env_path, .{}) catch continue;
                 defer temp_dir.close();
-                const std_zig = temp_dir.openFile("lib/std/std.zig", .{.mode = .read_only}) catch continue;
-                _ = temp_dir.realpathAlloc(path_fba.allocator(), "lib/std") catch unreachable;
+                const std_zig = temp_dir.openFile("lib/std/std.zig", .{ .mode = .read_only }) catch continue;
+                _ = temp_dir.realpathAlloc(path_fba.allocator(), "lib/std/std.zig") catch unreachable;
                 break :find_file std_zig;
             }
         }
-        _ = std.fs.cwd().realpathAlloc(path_fba.allocator(), GetDirOfPath(file_name)) catch |e| {
+        _ = std.fs.cwd().realpathAlloc(path_fba.allocator(), file_name) catch |e| {
             std.log.err("Cannot allocate memoery for the abs path where \"{s}\" is located", .{file_name});
             return e;
         };
-        break: find_file std.fs.cwd().openFile(file_name, .{.mode = .read_only}) catch |e| {
+        break :find_file std.fs.cwd().openFile(file_name, .{ .mode = .read_only }) catch |e| {
             std.log.err("Cannot open file: {s}", .{file_name});
             return e;
         };
@@ -376,31 +336,23 @@ pub fn main() !void {
     try bw.flush();
 
     try file_set.put(((try file.stat()).inode), {});
-    var fn_queue = FnQueue.init(allocator, null); 
+    var fn_queue = FnQueue.init(allocator, null);
     defer fn_queue.deinit();
     try stdout.print("recursive depth: {}\n", .{recursive_depth});
-    try SearchFile(
-        allocator, 
-        file, 
-        &fn_queue, 
-        &match_ast, match_fn, 
-        &path_fba, 
-        &file_set,
-        recursive_depth);
+    try SearchFile(allocator, file, &fn_queue, &match_ast, match_fn, &path_fba, &file_set, recursive_depth);
 
     try stdout.print("opening {s}\n", .{file_name});
     defer file.close();
 
-    
     // var buffer: [1]Ast.Node.Index = undefined;
     try stdout.print("Find {} candidates\n", .{fn_queue.count()});
     while (fn_queue.removeOrNull()) |fr| {
-        try stdout.print("{s} {s}\n", .{fr.location, fr.fnString});
+        try stdout.print("{s}{s}{s} {s}{s}{s}\n", .{ Color.KGRN, fr.location, Color.KRST, Color.KCYN, fr.fnString, Color.KRST });
         allocator.free(fr.fnString);
         allocator.free(fr.location);
         allocator.destroy(fr);
     }
-    
+
     // _ = token_tags;
     try bw.flush();
     // debugAst(match_ast);
@@ -411,18 +363,16 @@ fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
-
 fn debugAst(ast: Ast) void {
     const debug = std.log.debug;
     debug("[{}]Nodes", .{ast.nodes.len});
     for (ast.nodes.items(.tag), 0..) |t, i| {
-        debug("[{}]: {} = \"{s}\"", .{i,t, tokenRender(ast, ast.nodes.items(.main_token)[i])});
+        debug("[{}]: {} = \"{s}\"", .{ i, t, tokenRender(ast, ast.nodes.items(.main_token)[i]) });
     }
     // try stdout.print("token tags:\n", .{});
     debug("[{}]Tokens", .{ast.tokens.len});
     for (ast.tokens.items(.tag), 0..) |t, i| {
         if (t == .invalid) break;
-        debug("[{}]: {} = \"{s}\"", .{i, t, tokenRender(ast, @intCast(i)) });
-        
+        debug("[{}]: {} = \"{s}\"", .{ i, t, tokenRender(ast, @intCast(i)) });
     }
 }
